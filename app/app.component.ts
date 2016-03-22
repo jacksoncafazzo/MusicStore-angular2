8901eba352b01375540772d6d26aeb5f89cfa0d6
Dustin Davis and Jackson Cafazzo <hiitsdustindavis@gmail.com>
2016-03-22 12:00:28 -0700

    add click Event Emitter to h3

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,9 @@ import { Component } from 'angular2/core';
   template:`
     <div class="container">
       <h1>Skeleton Angular2 App!</h1>
-      <div *ngFor="#task of tasks">
-        <h3>{{ task.description }}</h3>
-        <h4> Hi, I'm another nested HTML element. There are 4 copies of me because we have 4 tasks.</h4>
-      </div>
+        <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
+          {{ task.description }}
+        </h3>
     </div>
     `
 })
@@ -20,11 +19,15 @@ export class AppComponent {
       new Task("Create To-Do List app.", 0),
       new Task("Learn Kung Fu.", 1),
       new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3)
+      new Task("Do the laundry.", 3),
       new Task("Go retrieve life giving device", 111)
     ];
   }
+  taskWasSelected(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
 }
+
 
 export class Task {
   public done: boolean = false;
