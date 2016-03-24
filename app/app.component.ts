d900e0dc3069202d78b11e62aec252c8091897cc
brooke hurford and jackson cafazzo <brookebhurford@gmail.com>
2016-03-24 09:12:27 -0700

    Will build album model and app component to be showing on the page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,34 +1,26 @@
-import { Component, EventEmitter } from 'angular2/core';
-import { TaskListComponent } from './task-list.component';
-import { Task } from './task.model';
-
+import { Component } from 'angular2/core';
+import { Album } from './album.model';
 
 @Component({
   selector: 'my-app',
-  directives: [TaskListComponent],
-  template:`
-    <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-      <task-list
-      [taskList]="tasks"
-      (onTaskSelect)="taskWasSelected($event)">
-      </task-list>
+  template: `
+  <div class="container">
+    <h1>Music Store</h1>
+    <div *ngFor="#album of albums">
+      <h3>{{ album.name }} : {{ album.artist }} \${{ album.price }}</h3>
+      <h4>{{ album.genre }}</h4>
     </div>
-    `
+  </div>
+  `
 })
 
 export class AppComponent {
-  public tasks: Task[];
-  constructor() {
-    this.tasks = [
-      new Task("Create To-Do List app.", 0),
-      new Task("Learn Kung Fu.", 1),
-      new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3),
-      new Task("Go retrieve life giving device", 111)
-    ];
-  }
-  taskWasSelected(clickedTask: Task): void {
-    console.log("Maude here:" + clickedTask);
+  public albums: Album[];
+  constructor(){
+    this.albums = [
+      new Album("Houses of the Holy", "Led Zepplin", 1, "Rock"),
+      new Album("The Big Roar", "Joy Formidable", 10, "Indie"),
+      new Album("Built on Glass", "Chet Faker", 10, "Australian")
+    ]
   }
 }
